81609d37e73dd931f621ab942d9530b7fac0417c
Ally <ally@primeacademy.io>
2018-04-04 14:35:59 -0500

    working on heroku deploy

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const apiRouter = require('./routes/api.router');
 
-let port = 5000 || process.env.PORT;
+let port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(express.static('server/public'));
