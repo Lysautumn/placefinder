da3d355dbdd24c42f71ba947565e7db3cb765f7b
Ally <ally@primeacademy.io>
2018-04-07 14:16:49 -0500

    refactored to add class

--- server/public/scripts/controllers/user.controller.js
+++ server/public/scripts/controllers/user.controller.js
@@ -1,7 +1,16 @@
+
 angular.module('placeApp').controller('UserController',['$mdDialog', '$mdToast', '$location', '$http', function($mdDialog, $mdToast, $location, $http) {
     console.log('User Controller');
     let self = this;
 
+    class Place {
+        constructor(name, address) {
+            this.name = name;
+            this.address = address;
+        }
+    }
+
+    self.formatPlaces = Place.formatPlaces;
     // Options available from Foursquare API
     self.searchOptions = ['food', 'drinks', 'coffee', 'shops', 'art', 'outdoors', 'sights', 'trending'];
 
@@ -13,7 +22,10 @@ angular.module('placeApp').controller('UserController',['$mdDialog', '$mdToast',
                 section: self.userOption
             }
         }).then(function(response) {
-            self.searchResults = response.data.response.groups[0].items;  
+            let responseArray = response.data.response.groups[0].items;
+            for(let i = 0; i < responseArray.length; i++) {
+                self.searchResults.push(new Place(responseArray[i].venue.name, responseArray[i].venue.location.address));
+            }    
         }).catch(function(error) {
             console.log('There was an error', error);
         });
